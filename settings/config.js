b9da3b5150126702b503da4826b715693c60a4b7
sombras1925 <81181609+sombras1925@users.noreply.github.com>
2021-08-23 21:04:28 +0200

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'ShanBot',
-        ownerName: 'Shan',
+        botName: 'MagicBot',
+        ownerName: 'Magic Dark',
         youtube: 'YOUTUBE_LINK',
         instagram: 'INSTAGRAM_LINK',
 }
